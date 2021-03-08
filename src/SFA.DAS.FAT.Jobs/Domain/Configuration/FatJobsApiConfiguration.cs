26f0b7bab11538c5d3199afdef81c5ece2694c97
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-03-08 20:01:59 +0000

    FAT2-425 - Initial commit for FAT web function
    Function to be used for removing expired shortlists

---
+++ src/SFA.DAS.FAT.Jobs/Domain/Configuration/FatJobsApiConfiguration.cs
@@ -0,0 +1,8 @@
+namespace SFA.DAS.FATJobs.Domain.Configuration
+{
+    public class FatJobsApiConfiguration
+    {
+        public string BaseUrl { get; set; }
+        public string Key { get; set; }
+    }
+}
---
+++ src/SFA.DAS.FAT.Jobs/Startup.cs
@@ -0,0 +1,57 @@
+using System;
+using System.IO;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using SFA.DAS.Configuration.AzureTableStorage;
+using SFA.DAS.FATJobs;
+using SFA.DAS.FATJobs.Domain.Configuration;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace SFA.DAS.FATJobs
+{
+    
+    public class Startup : FunctionsStartup
+    {
+        private IConfiguration _configuration;
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+            
+            var serviceProvider = builder.Services.BuildServiceProvider();
+            var configuration =  serviceProvider.GetService<IConfiguration>();
+            
+            var config = new ConfigurationBuilder()
+                .AddConfiguration(configuration)
+                .SetBasePath(Directory.GetCurrentDirectory())
+#if DEBUG
+                .AddJsonFile("local.settings.json", true)
+                .AddJsonFile("local.settings.Development.json", true)
+#endif
+                .AddEnvironmentVariables();
+
+            if (!configuration["EnvironmentName"].Equals("DEV", StringComparison.CurrentCultureIgnoreCase))
+            {
+                config.AddAzureTableStorage(options =>
+                    {
+                        options.ConfigurationKeys = configuration["ConfigNames"].Split(",");
+                        options.StorageConnectionString = configuration["ConfigurationStorageConnectionString"];
+                        options.EnvironmentName = configuration["EnvironmentName"];
+                        options.PreFixConfigurationKeys = false;
+                    }
+                );
+            }
+            
+            _configuration = config.Build();
+            builder.Services.Configure<FatJobsApiConfiguration>(_configuration.GetSection(nameof(FatJobsApiConfiguration)));
+            builder.Services.AddSingleton(cfg => cfg.GetService<IOptions<FatJobsApiConfiguration>>().Value);
+
+            builder.Services.AddSingleton(new FunctionEnvironment(configuration["EnvironmentName"]));
+
+            
+            builder.Services.BuildServiceProvider();
+        }
+    }
+}
