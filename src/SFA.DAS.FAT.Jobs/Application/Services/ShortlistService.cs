e58b141dac4a4c711634dadb97ad33841c6f5d69
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-04-30 16:14:50 +0100

    Add service for getting expired shortlist user ids

--- src/SFA.DAS.FAT.Jobs/Application/Services/ShortlistService.cs
+++ src/SFA.DAS.FAT.Jobs/Application/Services/ShortlistService.cs
@@ -1,7 +1,28 @@
-namespace SFA.DAS.FATJobs.Application.Services
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SFA.DAS.FAT.Jobs.Domain.Configuration;
+using SFA.DAS.FAT.Jobs.Domain.Interfaces;
+using SFA.DAS.FAT.Jobs.Infrastructure.Api.Requests;
+using SFA.DAS.FAT.Jobs.Infrastructure.Api.Responses;
+
+namespace SFA.DAS.FAT.Jobs.Application.Services
 {
     public class ShortlistService
     {
-        
+        private readonly IApiClient _apiClient;
+
+        public ShortlistService (IApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+        public async Task<IEnumerable<Guid>> GetExpiredShortlists()
+        {
+            var shortlistUserIds =
+                await _apiClient.Get<GetExpiredShortlistResponse>(
+                    new GetExpiredShortlistsRequest(ConfigurationConstants.ExpiryPeriodInDays));
+
+            return shortlistUserIds.UserIds;
+        }
     }
 }
---
+++ src/SFA.DAS.FAT.Jobs/Domain/Configuration/ConfigurationConstants.cs
@@ -0,0 +1,7 @@
+namespace SFA.DAS.FAT.Jobs.Domain.Configuration
+{
+    public static class ConfigurationConstants
+    {
+        public static uint ExpiryPeriodInDays = 30;
+    }
+}
